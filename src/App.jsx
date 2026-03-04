cdb64879dfef0e118b49b34442a9b1426e4bda02
TheLuis <luisdavidgilcabarcas47@gmail.com>
2026-03-04 17:46:04 -0500

    Inicio y estructuración del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import '../src/css/globals.css'
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+
+  return (
+    <div className="app">
+      <Header />
+      <main className="app__main">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </main>
+    </div>
+  )
+}
+
+export default App
